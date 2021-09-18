adf2ccf3a99b1f7eb298b1e1d026f309ba69b7da
Josephine Deng <jojod_27@yahoo.com>
2021-09-18 17:28:54 -0400

    connected jsonsearch to index.htmlgit add .! worked on color in js

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
 
     // move course to dictionary object respective to semester
-        // add to new object, remove from old object
+    // add to new object, remove from old object
     // calculate semester's new credit total
-        // if semester's new credit total > allowance based on ksas / wse, display warning
+    // if semester's new credit total > allowance based on ksas / wse, display warning
 }
 
 // let fall1 = {};
@@ -26,75 +26,84 @@ function drop(ev) {
 // let fall4 = {};
 // let spring4 = {};
 
-// function addCourse() {
-//     // if last 3 digits of code is 100 -> freshman fall; 200 -> sophomore fall, etc
-//     // add course to appropriate object
-//     // calculate semester's new credit total & total credit total
+// document.getElementById("fall1").innerHTML =
+        //     `<div class="card drag" id="drag6" draggable="true" ondragstart="drag(event)">
+        //         <button class="delete is-small" id="delete6" onclick="deleteCourse(6);"></button>
+        //         <div class="card-content">
+        //             course code<br>
+        //             course title (credits)
+        //         </div>
+        //     </div>`;
 
-//     // assigns color based on areas, also checks major req based on areas
-//     // h red, n green, s yellow, q purple, e blue
-//     let areas = ["H"];
-//     let r, g, b;
+// if last 3 digits of code is 100 -> freshman fall; 200 -> sophomore fall, etc
+    // add course to appropriate object
+    // calculate semester's new credit total & total credit total
+// assigns color based on areas, also checks major req based on areas
 
-//     if (areas.includes("H")) {
-//         r = 249;
-//         g = 111;
-//         b = 111;
-//         // #F96F6F
-//         // 249, 111, 111
-//     }
+function color() {
+    let areas = ['H', 'S'];
+    let r = 0;
+    let g = 0;
+    let b = 0;
 
-//     if (areas.includes("N")) {
-//         if (r != 0) {
-//             r = (r + 140) / 2;
-//             g = (g + 249) / 2;
-//             b = (b + 111) / 2;
-//         }
-//         // #8CF96F
-//         // 140, 249, 111
-//     }
+    if (areas.includes('H')) { // red
+        r = 249;
+        g = 111;
+        b = 111;
+    }
 
-//     if (areas.includes("S")) {
-//         if (r != 0) {
-//             r = (r + 249) / 2;
-//             g = (g + 235) / 2;
-//             b = (b + 111) / 2;
-//         }
-//         // #F9EB6F
-//         // 249, 235, 111
-//     }
+    if (areas.includes('N')) { // green
+        if (r != 0) {
+            r = (r + 91) / 2;
+            g = (g + 226) / 2;
+            b = (b + 108) / 2;
+        } else {
+            r = 91;
+            g = 226;
+            b = 108;
+        }
+    }
 
-//     if (areas.includes("Q")) {
-//         if (r != 0) {
-//             r = (r + 194) / 2;
-//             g = (g + 112) / 2;
-//             b = (b + 255) / 2;
-//         }
-//         // #C270FF
-//         // 194, 112, 255
-//     }
+    if (areas.includes('S')) { // yellow
+        if (r != 0) {
+            r = (r + 255) / 2;
+            g = (g + 246) / 2;
+            b = (b + 107) / 2;
+        } else {
+            r = 255;
+            g = 246;
+            b = 107;
+        }
 
-//     if (areas.includes("E")) {
-//         if (r != 0) {
-//             r = (r + 112) / 2;
-//             g = (g + 212) / 2;
-//             b = (b + 255) / 2;
-//         }
-//         // #70D4FF
-//         // 112, 212, 255
-//     }
-    
-//     // document.getElementById("fall1").innerHTML +=
-//     //     `<div class="card drag" id="drag6" draggable="true" ondragstart="drag(event)">
-//     //         <button class="delete is-small" id="delete6" onclick="deleteCourse(6);"></button>
-//     //         <div class="card-content">
-//     //             course code<br>
-//     //             course title (credits)
-//     //         </div>
-//     //     </div>`;
-// }
+    }
 
-// addCourse();
+    if (areas.includes('Q')) { // purple
+        if (r != 0) {
+            r = (r + 194) / 2;
+            g = (g + 112) / 2;
+            b = (b + 255) / 2;
+        } else {
+            r = 194;
+            g = 112;
+            b = 255;
+        }
+    }
+
+    if (areas.includes('E')) { // blue
+        if (r != 0) {
+            r = (r + 112) / 2;
+            g = (g + 212) / 2;
+            b = (b + 255) / 2;
+        } else {
+            r = 112;
+            g = 212;
+            b = 255;
+        }
+    }
+
+    // document.getElementById("drag1").style.color = "white";
+    document.getElementById("drag1").style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+}
 
 function deleteCourse(id) {
     document.getElementById("drag" + id).style.display = "none";
